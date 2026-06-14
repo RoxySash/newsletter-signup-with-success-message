f35e26c28c8b1fa288936912fa73f2e89489f07f
Roxane Hedge <roxanehedge@gmail.com>
2026-06-14 11:02:24 -0500

    adding modal

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 const email = document.getElementById('email');
 const form = document.getElementById('form');
 const submitBtn = document.getElementById('submit-btn');
+const modal = document.getElementById('modal');
 
 function handleSubmit(e) {
-    e.preventDefault()
+    e.preventDefault();
+    const data = Object.fromEntries(new FormData(e.target));
 }
 
 form.addEventListener('submit', handleSubmit);
 
 submitBtn.addEventListener("click", function(){
-    alert('Button Clicked');
+  //  alert('Button Clicked');
+    modal.showModal();
+    // add the function for the error or change modal 
 });
 
 
